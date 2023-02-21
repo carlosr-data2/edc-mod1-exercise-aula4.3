867b42c8c2045176130a540a5a52c23f98cf6d6b
crojas02 <carlos.tardio03@gmail.com>
2023-02-21 14:37:36 -0300

    h

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "dl" {
-  bucket = "datalake-ney-igti-edc-tf"
+  bucket = "datalake-carlos2-igti-edc-tf"
   acl    = "private"
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_s3_bucket" "dl" {
 
 
 resource "aws_s3_bucket" "stream" {
-  bucket = "igti-ney-streaming-bucket"
+  bucket = "igti-carlos2-streaming-bucket"
   acl    = "private"
 
   tags = {
