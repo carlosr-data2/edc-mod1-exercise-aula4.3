11e6c3b253310909961494453ffdcf480a9e3a3d
crojas02 <carlos.tardio03@gmail.com>
2023-02-21 13:49:11 -0300

    backend tf s3

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -4,10 +4,13 @@ provider "aws" {
 
 
 # Centralizar o arquivo de controle de estado do terraform
+# somente aqui quando estamos definindo o backend do terraform, não conseguimos usar variaveis aqui
+# e não conseguimos criar esse bucket pelo terraform, ele tem que já estar criado
+# terraform.tfstate é um arquivo json que guardada o estada de cada recurso que é subido pelo terraform
 terraform {
   backend "s3" {
-    bucket = "terraform-state-igti-ney"
+    bucket = "terraform-state-igti-carlos"
     key    = "state/igti/edc/mod1/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
